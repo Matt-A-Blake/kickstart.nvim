594d7d8d8bbedd69f51299abb12f997981904c4e
Matt Blake <mblake@neurologica.com>
2025-04-18 19:18:12 -0400

    Moved harpoon and orgmode to the lub/plugins directory.

--- init.lua
+++ init.lua
@@ -979,37 +979,11 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
-  -- Nvim Orgmode plugin
-  {
-    'nvim-orgmode/orgmode',
-    dependencies = {
-      'nvim-telescope/telescope.nvim',
-      'nvim-orgmode/telescope-orgmode.nvim',
-      'nvim-orgmode/org-bullets.nvim',
-    },
-    event = 'VeryLazy',
-    config = function()
-      -- Setup orgmode
-      require('orgmode').setup {
-        org_agenda_files = '~/orgfiles/**/*',
-        org_default_notes_file = '~/orgfiles/refile.org',
-      }
-      -- Setup org-bullets
-      require('org-bullets').setup()
-      require('telescope').setup()
-      require('telescope').load_extension 'orgmode'
-      vim.keymap.set('n', '<leader>r', require('telescope').extensions.orgmode.refile_heading)
-      vim.keymap.set('n', '<leader>fh', require('telescope').extensions.orgmode.search_headings)
-      vim.keymap.set('n', '<leader>li', require('telescope').extensions.orgmode.insert_link)
-    end,
-  },
-  {
-    'ThePrimeagen/harpoon',
-    branch = 'harpoon2',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-  },
+  -- Personal plugins.
+  require 'plugins.harpoon',
+  require 'plugins.orgmode',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
---
+++ lua/plugins/orgmode.lua
@@ -0,0 +1,24 @@
+-- Nvim Orgmode plugin
+return {
+  'nvim-orgmode/orgmode',
+  dependencies = {
+    'nvim-telescope/telescope.nvim',
+    'nvim-orgmode/telescope-orgmode.nvim',
+    'nvim-orgmode/org-bullets.nvim',
+  },
+  event = 'VeryLazy',
+  config = function()
+    -- Setup orgmode
+    require('orgmode').setup {
+      org_agenda_files = '~/orgfiles/**/*',
+      org_default_notes_file = '~/orgfiles/refile.org',
+    }
+    -- Setup org-bullets
+    require('org-bullets').setup()
+    require('telescope').setup()
+    require('telescope').load_extension 'orgmode'
+    vim.keymap.set('n', '<leader>r', require('telescope').extensions.orgmode.refile_heading)
+    vim.keymap.set('n', '<leader>fh', require('telescope').extensions.orgmode.search_headings)
+    vim.keymap.set('n', '<leader>li', require('telescope').extensions.orgmode.insert_link)
+  end,
+}
