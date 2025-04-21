8a39a01485e2e63b8daa79755c11d5dd3a14f191
Matt Blake <mblake@neurologica.com>
2025-04-21 13:18:36 -0400

    Fixed a couple of fold setting typos.

--- lua/config/fold.lua
+++ lua/config/fold.lua
@@ -5,5 +5,5 @@ local opt = vim.opt
 opt.foldmethod = 'expr'
 opt.foldexpr = 'nvim_tresetter#foldexpr()'
 opt.foldlevel = 99
-opt.follevelstart = 2
-opt.flodnestmax = 5
+opt.foldlevelstart = 2
+opt.foldnestmax = 5
