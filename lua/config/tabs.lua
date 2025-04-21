cba5225c1184e9d72babedc40c4ba9b2308775df
Matt Blake <mblake@neurologica.com>
2025-04-21 12:41:03 -0400

    Updates to tabs.

--- lua/config/tabs.lua
+++ lua/config/tabs.lua
@@ -1,4 +1,6 @@
-vim.o.tabstop = 4
-vim.o.shiftwidth = 4
+local TAB_WIDTH = 4
+vim.o.tabstop = TAB_WIDTH
+vim.o.shiftwidth = TAB_WIDTH
+vim.o.shiftwidth = TAB_WIDTH
 vim.o.expandtab = true
 vim.o.smartindent = true
