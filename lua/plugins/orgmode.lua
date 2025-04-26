ab58e4f484fb97374ef69321f5249ff3f8ec4d2a
Matt Blake <mblake@neurologica.com>
2025-04-25 20:27:45 -0400

    Updates to orgmode bullets and added key descriptions.

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -14,11 +14,21 @@ return {
       org_default_notes_file = '~/orgfiles/refile.org',
     }
     -- Setup org-bullets
-    require('org-bullets').setup()
+    require('org-bullets').setup {
+      symbols = {
+        list = '',
+        headlines = { '', '', '', '' },
+        checkboxes = {
+          half = { '󰐋', '@org.checkbox.halfchecked' },
+          done = { '', '@org.keyword.done' },
+          todo = { '˟', '@org.keyword.todo' },
+        },
+      },
+    }
     require('telescope').setup {}
     require('telescope').load_extension 'orgmode'
-    vim.keymap.set('n', '<leader>r', require('telescope').extensions.orgmode.refile_heading)
-    vim.keymap.set('n', '<leader>fh', require('telescope').extensions.orgmode.search_headings)
-    vim.keymap.set('n', '<leader>li', require('telescope').extensions.orgmode.insert_link)
+    vim.keymap.set('n', '<leader>r', require('telescope').extensions.orgmode.refile_heading, { desc = 'Org: [R]efile Heading' })
+    vim.keymap.set('n', '<leader>fh', require('telescope').extensions.orgmode.search_headings, { desc = 'Org: Search Heading' })
+    vim.keymap.set('n', '<leader>li', require('telescope').extensions.orgmode.insert_link, { desc = 'Org: [L]ink [I]nsert' })
   end,
 }
