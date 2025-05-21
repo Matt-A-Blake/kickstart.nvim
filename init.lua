a8b7f84fefcde74349f1986bb6eb5dbec063f405
Matt Blake <mblake@neurologica.com>
2025-05-21 15:49:31 -0400

    Added hex.nvim

--- init.lua
+++ init.lua
@@ -977,6 +977,7 @@ require('lazy').setup({
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
+    { 'RaafatTurki/hex.nvim' },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
     --
