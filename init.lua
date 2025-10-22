49e9617ee275513b823c404b211af7b8834ceb69
Matt Blake <mblake@neurologica.com>
2025-10-22 18:49:11 -0400

    Added Tabby plugin.

--- init.lua
+++ init.lua
@@ -999,6 +999,79 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
+  {
+    'nanozuki/tabby.nvim',
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      local theme = {
+        fill = 'TabLineFill',
+        head = 'TabLine',
+        current_tab = 'TabLineSel',
+        tab = 'TabLine',
+        win = 'TabLine',
+        tail = 'TabLine',
+      }
+      require('tabby').setup {
+        line = function(line)
+          return {
+            {
+              { '  ', hl = { fg = '#7FBBB3', bg = '#414B50' } },
+              line.sep('', theme.head, theme.fill),
+            },
+            line.tabs().foreach(function(tab)
+              local hl = tab.is_current() and theme.current_tab or theme.tab
+
+              -- remove count of wins in tab with [n+] included in tab.name()
+              local name = tab.name()
+              local index = string.find(name, '%[%d')
+              local tab_name = index and string.sub(name, 1, index - 1) or name
+
+              -- indicate if any of buffers in tab have unsaved changes
+              local modified = false
+              local win_ids = require('tabby.module.api').get_tab_wins(tab.id)
+              for _, win_id in ipairs(win_ids) do
+                if pcall(vim.api.nvim_win_get_buf, win_id) then
+                  local bufid = vim.api.nvim_win_get_buf(win_id)
+                  if vim.api.nvim_buf_get_option(bufid, 'modified') then
+                    modified = true
+                    break
+                  end
+                end
+              end
+
+              return {
+                line.sep('', hl, theme.fill),
+                tab.number(),
+                tab_name,
+                modified and '',
+                tab.close_btn '',
+                line.sep('', hl, theme.fill),
+                hl = hl,
+                margin = ' ',
+              }
+            end),
+            line.spacer(),
+            {
+              line.sep('', theme.tail, theme.fill),
+              { '  ', hl = theme.tail },
+            },
+            hl = theme.fill,
+          }
+        end,
+      }
+      vim.api.nvim_set_keymap('n', '<leader>ta', ':$tabnew<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<leader>tc', ':tabclose<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<leader>to', ':tabonly<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<leader>tn', ':tabn<CR>', { noremap = true })
+      vim.api.nvim_set_keymap('n', '<leader>tp', ':tabp<CR>', { noremap = true })
+      -- move current tab to previous position
+      vim.api.nvim_set_keymap('n', '<leader>tmp', ':-tabmove<CR>', { noremap = true })
+      -- move current tab to next position
+      vim.api.nvim_set_keymap('n', '<leader>tmn', ':+tabmove<CR>', { noremap = true })
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+    end,
+  },
+
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
