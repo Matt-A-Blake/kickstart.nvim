f420c96cc4a63b04b9836ab165c4c57513254ec5
Matt Blake <mblake@neurologica.com>
2026-08-06 19:20:06 -0400

    Had Claude add the claudecode.nvim module and some doc / cheat.txt help updates.

--- init.lua
+++ init.lua
@@ -1169,6 +1169,7 @@ require('lazy').setup({
   require 'plugins.ufo',
   --require 'plugins.hex',
   require 'plugins.neo-tree',
+  require 'plugins.claudecode',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
